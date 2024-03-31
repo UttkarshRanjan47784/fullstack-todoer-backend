82135d6a019378f3eb4cc3787a722e189d4b3275
Uttkarsh Ranjan <utr.1206@gmail.com>
2024-03-31 22:59:02 +0530

    Added Password Encryption using bcrypt

--- auth.js
+++ auth.js
@@ -1,11 +1,13 @@
 import jwt from 'jsonwebtoken'
 import mongoose, { mongo } from 'mongoose'
 import env from "dotenv"
+import bcrypt from "bcrypt"
 
 env.config();
 
 const url = process.env.MONGO_URL;
 const secret = process.env.SERVER_SECRET;
+const salt = process.env.SALT_ROUNDS;
 
 const userSchema = new mongoose.Schema({
     username : String,
@@ -21,55 +23,74 @@ async function loginUser (req, res) {
             username : req.body["username"]
         });
         if (temp == null || temp == undefined)
-            throw new Error(`User does not exist!`);
-        if (req.body['password'] == temp.password){
-            let token =  jwt.sign({
-                username : req.body["username"]
-            }, secret)
-            console.log(`token generated`)
-            res.json({
-                msg : true,
-                token : token
-            });
-        }
-        else
-            throw new Error(`Invalid Password`);
+            res.json(`User does not exist!`);
+
+        bcrypt.compare(req.body["password"], temp.password, function(err, result) {
+            if (err){
+                 res.json(err.message)
+            }
+            if (result) {
+                let token =  jwt.sign({
+                    username : req.body["username"]
+                }, secret);
+                res.json({
+                    msg : true,
+                    token : token
+                });
+            }
+            else 
+                res.json(`Invalid Password`)
+        });
     } catch (error) {
         res.json(error.message)
     }
-    mongoose.connection.close();
+    await mongoose.connection.close();
 }
 
 async function registerUser (req, res) {
     try {
-        await mongoose.connect(url);
-        let user = new userModel(req.body);
-        
-        let temp = await userModel.findOne({
-            username : req.body["username"]
+        bcrypt.hash(req.body["password"], Number(salt), async (err, hash) => {
+            if (err){
+                console.log(err.message)
+                res.send({
+                    stat : `fail`,
+                    msg : err.message
+                });
+            }
+            else {
+                await mongoose.connect(url);
+                let temp = await userModel.findOne({
+                    username : req.body["username"]
+                });
+                if (temp != null || temp != undefined){
+                    res.send({
+                        stat : `fail`,
+                        msg : `User already exists`
+                    });
+                    return
+                }
+                    
+                let user = new userModel({
+                    username : req.body["username"],
+                    password : hash
+                });
+                await user.save();
+                let token = jwt.sign({
+                    username : req.body["username"]
+                }, secret)
+                res.json({
+                    stat : `pass`,
+                    msg : token
+                });  
+            }        
         });
-
-        if (temp != null || temp != undefined)
-            throw new Error(`User already exists`);
-
-        await user.save();
-
-        let token = jwt.sign({
-            username : req.body["username"]
-        }, secret)
-
-        res.json({
-            stat : `pass`,
-            msg : token
-        });
-
     } catch (error) {
         res.send({
             stat : `fail`,
             msg : error.message
         });
     }
-    mongoose.connection.close();
+    await mongoose.connection.close();
 }
 
 export { registerUser, loginUser }
